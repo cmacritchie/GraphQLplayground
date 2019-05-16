80cc150fecd3664fd918c243c538fdbba6e438d0
Craig MacRitchie <craig.macritchie@edu.sait.ca>
2019-05-15 22:22:26 -0600

    finished server side, working on client side

---
+++ section 2/client/components/SongList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag'; //helper to write queries
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+    renderSongs() {
+
+        if(this.props.data.loading){return <div>loading...</div>;}
+
+        return this.props.data.songs.map(song=>{
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            );
+        });
+    }
+
+
+    render() {
+        return (
+        <ul className="collection">
+            {this.renderSongs()}
+        </ul>
+        )
+    }
+}
+
+const query = gql`
+    {
+        songs{
+            id
+            title
+        } 
+    }
+`;
+
+export default graphql(query)(SongList);
